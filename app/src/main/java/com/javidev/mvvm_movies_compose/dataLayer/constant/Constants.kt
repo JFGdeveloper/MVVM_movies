2c8903ea6612a12b1b3701ff470fd53c7d3809ed
Javier fernandez <javierfernandezdeveloper@gmail.com>
2022-03-08 01:32:02 +0100

    crado la clase de constantes

---
+++ app/src/main/java/com/javidev/mvvm_movies_compose/dataLayer/constant/Constants.kt
@@ -0,0 +1,9 @@
+package com.javidev.mvvm_movies_compose.dataLayer.constant
+
+class Constants {
+    companion object{
+        const val API_BASE_URL= ""
+        const val IMAGE_BASE_URL= ""
+        const val API_KEY= ""
+    }
+}
